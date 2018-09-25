0f34ba7b010401fedde85db2c98e68da90760e19
Hadjer <fh_benmeziane@esi.dz>
2018-09-25 07:23:31 +0000

    Add files via upload

--- multiple-hosts.py
+++ multiple-hosts.py
@@ -1,15 +1,17 @@
-from pyzabbix import ZabbixAPI
+from zabbix_api import ZabbixAPI
 import csv
 import sys
+import getpass
 
 # Extract arguments 
 frontend = sys.argv[1]
-user = sys.argv[2]
-password = sys.argv[3]
+
+user = input('Username : ')
+password = getpass.getpass('Password : ')
 
 
 # Zabbix server IP address
-zapi = ZabbixAPI(frontend)
+zapi = ZabbixAPI(server=frontend)
 
 # Zabbix Auth config for the Web frontEnd 
 zapi.session.auth = (user, password)
@@ -23,101 +25,122 @@ zapi.login(user, password)
 print("Connected to Zabbix API Version %s" % zapi.api_version())
 
 
-#Read csv file (hostname;IpAddress)
+#Read csv file (IpAddress;visibleName)
 f = csv.reader(open('hosts.csv'), delimiter=';')
-for [hostname,ip1,ip2,ip3] in f:
+for [ip,visibleName] in f:
     # Create host 
-    hostcriado = zapi.host.create(
-            host= hostname,
-            status= 0,
-            interfaces=[{
+    print(ip)
+    print(visibleName)
+    hostcriado = zapi.host.create({
+            'host' : ip,
+            'name' : visibleName, 
+            'status' : 0,
+            # Interfaces definitions 
+            'interfaces' : [{
                 "type": 2,
                 "main": "1",
                 "useip": 1,
-                "ip": ip1,
-                "dns": "",
-                "port": 161
-            }, 
-            {
-                "type": 2,
-                "main": "2",
-                "useip": 1,
-                "ip": ip2,
-                "dns": "",
-                "port":161
-            }, 
-            {
-                "type": 2,
-                "main": "3",
-                "useip": 1,
-                "ip": ip3,
+                "ip": ip,
                 "dns": "",
                 "port": 161
             }],
-            groups=[{
-                "groupid": 2
-            }],
-            templates=[{
-                "templateid": 10249
-            }], 
-            
-        )
+            'groups' : [{
+                "groupid": 13
+            }]    
+        })
     print(hostcriado['hostids'][0])
 
     # Check for the interface ID 
-    host_interface = zapi.hostinterface.get(filter={"hostid": hostcriado['hostids'][0]} )
+    host_interface = zapi.hostinterface.get({'filter' : {"hostid": hostcriado['hostids'][0]}} )
     if host_interface:
         interface = host_interface[0]
         print(interface)
     else:
         print("no interface found") 
 
-    for i in host_interface :  
-        itemtest = zapi.item.create(
-            name  = "In-" + i['ip'],
-            key_= "KeyIn-"+i['ip'],
-            hostid = hostcriado['hostids'][0] ,
-            type =  4,
-            snmp_oid= "IF-MIB::ifInOctets", 
-            snmp_community = "public", 
-            value_type =  3,
-            interfaceid =  i["interfaceid"], 
-            delay = "30s"
-        )
+    itemtest = zapi.item.create({
+        'name'  : "GigaEthernet 8-In",
+        'key_' : "GigaEthernet8-In",
+        'hostid' : hostcriado['hostids'][0],
+        'type' :  4,
+        'snmp_oid' : "1.3.6.1.2.1.2.2.1.10.1", 
+        'snmp_community' : "N@ftal-RO", 
+        'value_type' :  3,
+        'interfaceid' :  interface['interfaceid'], 
+        'delay' : "30s"
+    })
 
-        itemtest = zapi.item.create(
-            name  = "Out-"+ i['ip'],
-            key_= "KeyOut-" + i['ip'],
-            hostid = hostcriado['hostids'][0] ,
-            type =  4,
-            snmp_oid= "IF-MIB::ifOutOctets", 
-            snmp_community = "public", 
-            value_type =  3,
-            interfaceid =  i["interfaceid"], 
-            delay = "30s"
-        )
+    itemtest = zapi.item.create({
+        'name'  : "GigaEthernet8-Out",
+        'key_' : "GigaEthernet8-Out",
+        'hostid' : hostcriado['hostids'][0] ,
+        'type' :  4,
+        'snmp_oid' : "1.3.6.1.2.1.2.2.1.16.1", 
+        'snmp_community' : "N@ftal-RO", 
+        'value_type' :  3,
+        'interfaceid' :  interface['interfaceid'], 
+        'delay' : "30s"
+    })
+
+    itemtest = zapi.item.create({
+        'name'  : "GigaEthernet 9-In",
+        'key_' : "GigaEthernet9-In",
+        'hostid' : hostcriado['hostids'][0],
+        'type' :  4,
+        'snmp_oid' : "1.3.6.1.2.1.2.2.1.10.2", 
+        'snmp_community' : "N@ftal-RO", 
+        'value_type' : 3,
+        'interfaceid' :   interface['interfaceid'], 
+        'delay' : "30s"
+    })
+
+    itemtest = zapi.item.create({
+        'name'  : "GigaEthernet 9-Out",
+        'key_': "GigaEthernet9-Out",
+        'hostid' : hostcriado['hostids'][0],
+        'type' :  4,
+        'snmp_oid' :  "1.3.6.1.2.1.2.2.1.16.2", 
+        'snmp_community':  "N@ftal-RO", 
+        'value_type' : 3,
+        'interfaceid' : interface['interfaceid'], 
+        'delay' : "30s"
+    })
+
+    # Get Item IDs
+    host_items = zapi.item.get({'filter' : {"hostid": hostcriado['hostids'][0]}} )
+    if host_items:
+        items = host_items[0]
+        print(items)
+    else:
+        print("no item found") 
+    print(host_items)
+    # Create Graphs 
+    graph = zapi.graph.create({
+        'name' : "I/O-8",
+        'width' : 900, 
+        'height':  200, 
+        'gitems' : [{
+            "itemid" : host_items[0]['itemid'], 
+            "color" :"00AA00"
+        },{
+            "itemid" : host_items[1]['itemid'], 
+            "color" :"DD0000" 
+        }]
+    })
+
+    graph = zapi.graph.create({
+        'name'  :  "I/O-9",
+        'width' : 900, 
+        'height'  : 200, 
+        'gitems' : [{
+            "itemid" : host_items[2]['itemid'], 
+            "color" :"00AA00"
+        },{
+            "itemid" : host_items[3]['itemid'], 
+            "color" :"DD0000" 
+        }]
+    })
         
-        # Get Item IDs
-        host_items = zapi.item.get(filter={"hostid": hostcriado['hostids'][0]} )
-        if host_items:
-            items = host_items[0]
-            print(items)
-        else:
-            print("no item found") 
-
-        # Create Graphs 
-        graph = zapi.graph.create(
-            name  = "bandwidth"+i['ip'],
-            width=900, 
-            height = 500, 
-            gitems = [{
-                "itemid" : host_items[0]['itemid'], 
-                "color" :"00AA00"
-            },{
-                "itemid" : host_items[1]['itemid'], 
-                "color" :"00AA00" 
-            }]
-        )
 
 
     
